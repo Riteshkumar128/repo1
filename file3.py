0a60014f5263a27002c270103e4b99cad68464c8
Riteshkumar128 <choudhariritesh379@gmail.com>
2025-11-03 08:13:25 +0530

    Create file3.py

---
+++ file3.py
@@ -0,0 +1,9 @@
+git clone https://github.com/<tumhara-username>/PullRequestDemo.git
+cd PullRequestDemo
+git checkout -b feature-1
+echo "This is my Pull Request practical" > pr-demo.txt
+
+git add .
+git commit -m "Added pr-demo.txt file for pull request demo"
+git push origin feature-1
+go to github and create a pull request
