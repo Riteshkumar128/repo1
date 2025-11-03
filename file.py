464f3fbf99a888f73ccef306c25a716bf8e15402
Riteshkumar128 <choudhariritesh379@gmail.com>
2025-11-03 07:46:36 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,20 @@
-print("hello");
+bash        # switch to Bash
+pwsh        # switch back to PowerShell
+date   #nash
+Get-Date #pwsl
+
+az upgrade          # Update the CLI
+az version          # Check version
+
+az interactive
+
+exit
+
+az login --use-device-code
+az account list --output table
+az account set --subscription "<your-subscription-name>"
+az group create --name ritesh-rg --location centralindia
+az storage account create --name riteshstorage12345 --resource-group ritesh-rg --location centralindia --sku Standard_LRS
+az resource list --resource-group ritesh-rg --output table
+az group delete --name ritesh-rg --yes --no-wait
+
